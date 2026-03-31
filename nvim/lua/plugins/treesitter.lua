ebad7c054c428d8425fc468af8d8c98d90ddc98d
Pavel <tessmanpavel@gmail.com>
2026-03-31 20:58:30 +0300

    Pin tree-sitter branch to master

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 local M = {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
