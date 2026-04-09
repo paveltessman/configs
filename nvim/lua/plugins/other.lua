bd689069eb4b85a7afbbd81b7fb20beba7589a04
Pavel <tessmanpavel@gmail.com>
2026-04-09 23:13:57 +0300

    Remove unused plugins

--- nvim/lua/plugins/other.lua
+++ nvim/lua/plugins/other.lua
@@ -1,14 +1,8 @@
 return {
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
-  --
-    -- menu Autocompletion
+
+  -- menu Autocompletion
   'gelguy/wilder.nvim',
 
   -- "gc" to comment visual regions/lines
