3bbd3314f811549a349e44f182537e31ce35bf0d
Pavel <tessmanpavel@gmail.com>
2026-04-09 23:28:24 +0300

    Add all LSPs to `ensure_installed`

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,11 +1,11 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  commit = "0b9bb925c000ae649ff7e7149c8cd00031f4b539",
-  config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls" },
-      automatic_enable = false,
-    })
-  end
+    "williamboman/mason-lspconfig.nvim",
+    commit = "0b9bb925c000ae649ff7e7149c8cd00031f4b539",
+    config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "pyright", "ts_ls", "gopls", "asm_lsp" },
+            automatic_enable = false,
+        })
+    end
 
 }
