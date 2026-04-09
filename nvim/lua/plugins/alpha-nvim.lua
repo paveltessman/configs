7eac694b228455c5c90b88c4348da1ab6f7a9c5b
Pavel <tessmanpavel@gmail.com>
2026-04-09 23:23:56 +0300

    Disable icons on the welcome screen

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -3,6 +3,10 @@ return {
     'goolord/alpha-nvim',
     commit = "3979b01cb05734331c7873049001d3f2bb8477f4",
     config = function()
-        require 'alpha'.setup(require 'alpha.themes.startify'.config)
+        local startify = require("alpha.themes.startify")
+        startify.file_icons.enabled = false
+        require("alpha").setup(
+            startify.config
+        )
     end
 };
