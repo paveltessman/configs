dfe7fddf751833d0a08fa708f981c62e0465d81a
Pavel <tessmanpavel@gmail.com>
2026-09-12 12:13:28 +0300

    scripts: do not start tmux session if directory does not exists

--- scripts/start-tmux.sh
+++ scripts/start-tmux.sh
@@ -4,16 +4,21 @@ set -euo pipefail
 SESSION_NAME="$1"
 SESSION_DIR="$HOME/workspace/$SESSION_NAME"
 
+if [[ ! -d "$SESSION_DIR" ]]; then
+    echo "no such directory: ~/workspace/$SESSION_NAME"
+    exit 1
+fi
+
 retc=0
 
-tmux new-session -d -s $SESSION_NAME -c $SESSION_DIR
+tmux new-session -d -s "$SESSION_NAME" -c "$SESSION_DIR"
 
-tmux send-keys -t $SESSION_NAME:1 'dev' C-m
-tmux send-keys -t $SESSION_NAME:1 'nvim' C-m
+tmux send-keys -t "$SESSION_NAME":1 'dev' C-m
+tmux send-keys -t "$SESSION_NAME":1 'nvim' C-m
 
-tmux new-window -t $SESSION_NAME:2 -n 'zsh' -c $SESSION_DIR
-tmux send-keys -t $SESSION_NAME:2 'dev' C-m
-tmux send-keys -t $SESSION_NAME:2 'clear' C-m
+tmux new-window -t "$SESSION_NAME":2 -n 'zsh' -c "$SESSION_DIR"
+tmux send-keys -t "$SESSION_NAME":2 'dev' C-m
+tmux send-keys -t "$SESSION_NAME":2 'clear' C-m
 
 tmux detach 2> /dev/null || retc=$?
-tmux attach -t $SESSION_NAME
+tmux attach -t "$SESSION_NAME"
